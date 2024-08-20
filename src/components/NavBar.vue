396b243276d64335d2ec2cde467f930c00b4d230
Nizaam Jeftha <nizaam01.jeftha@gmail.com>
2024-08-21 01:16:16 +0200

    logoupdate

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
         <div class="container-fluid">
             <router-link class="navbar-brand" to="/">
-                <img src="https://jefthanizaam01.github.io/favoriteimages/images/retrovisionlogo.png" loading="lazy" alt="logo">
+                <img src="https://jefthanizaam01.github.io/favoriteimages/images/logo21.png" loading="lazy" alt="logo">
             </router-link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
